6865484b154ab92871acfa0c8484da7b3f8fe0a0
PC-Home <tonigadeamorant@gmail.com>
2021-03-01 12:41:53 +0100

    Refactor

---
+++ wwwtoni/public/src/procesarLogin.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+if(isset($_POST['email'])){
+    $email = $_POST['email'];
+    }
+if(isset($_POST['password'])){
+    $password = $_POST['password'];
+}
+
+$servidor = '127.0.0.1';
+$usuari = '2daw06_daw';
+$contrasenya = '10261259';
+$db = 'hosting_tonigadea';
+
+$conexio = new mysqli($servidor, $usuari, $contrasenya, $db);
+
+if($conexio->connect_error){
+    die ("Error de connexió:".$conexio->connect_error);
+}
+
+$sql = "SELECT clauusuari FROM users WHERE correu='$email'";
+$resultat = $conexio->query($sql);
+
+if($resultat->num_rows > 0){
+    while($row = $resultat->fetch_assoc()) {
+        $hash = implode("",$row);
+        if(password_verify($password,$hash)){
+            $dia = date("Y")."/".date("n")."/".date("j");
+            $hora = date("G").":".date("i").":".date("s");
+            $fp = fopen("../../log/usuaris.log","a");
+            fputs($fp, "Usuari accedeix al sistema: $email - Dia: $dia - Hora: $hora \n");
+            fclose($fp);
+
+            $_SESSION['user'] = $email;
+            header('Location: /index.php');
+            exit();
+        }
+      }  
+}
+$dia = date("Y")."/".date("n")."/".date("j");
+$hora = date("G").":".date("i").":".date("s");
+$fp = fopen("../../log/usuaris.log","a");
+fputs($fp, "Intent de acces al sistema: $email - Dia: $dia - Hora: $hora \n");
+fclose($fp);
+
+    header('Location: /public/registrado.php?error=Email o contraseña no valida');
+    exit();
+
+
+$conexio->close();
+?>
---
+++ wwwtoni/templates/nav.php
@@ -0,0 +1,45 @@
+<?php session_start() ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-white" style="font-size: 1.5em">
+<div class="container-fluid mx-5 px-5 pt-1">
+    <!--Logo-->
+    <a class="navbar-brand" href="/index.php">
+        <img src="/public/img/logo.png" width="165" height="84" class="d-inline-block align-top" alt="logo">
+    </a>
+    <!--End Logo-->
+
+    <!--Links-->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+                <a class="nav-link px-4" href="/index.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link px-4" href="/public/admin.php">Administrador</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link px-4" href="#">Resources</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link px-4" href="#">FAQ</a>
+            </li>
+            <?php
+            if(isset($_SESSION['user'])){echo "<li  class='nav-item'><a class='nav-link px-4 btn btn-danger text-white' href='/public/src/logout.php'>LogOut</a></li>";}
+            ?>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="/public/registrado.php" class="btn btn-rounded btn-azul text-white">
+                <?php
+                    if(isset($_SESSION['user'])){
+                        echo "Ver perfil";
+                    }else{
+                        echo "Iniciar sesión";
+                    }
+                ?>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!--End Links-->
+</div>
+</nav>
